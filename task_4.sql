92f7a3de6d3aecb93f1082511c3c3739769e73a2
mrt <iquixe@gmail.com>
2024-07-27 00:01:51 +0300

    fix

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,7 @@
 USE alx_book_store;
+
+SELECT "COLUMN_NAME", "COLUMN_TYPE", "SELECT"
+FROM "INFORMATION_SCHEMA.COLUMNS" 
+WHERE "TABLE_SCHEMA = 'alx_book_store'" 
+      AND 
+      "TABLE_NAME = 'Books'"
