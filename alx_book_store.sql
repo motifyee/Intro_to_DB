c7b71719551f9d8c38c921154c59be339270312c
mrt <iquixe@gmail.com>
2024-07-26 23:35:41 +0300

    fix

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Books(
-  book_id INT Primary Key,
+  book_id INT PRIMARY KEY,
   title VARCHAR(130),
   author_id INT, 
   price FLOAT,
@@ -11,26 +11,26 @@ CREATE TABLE Books(
 
 
 CREATE TABLE Authors(
-  author_id Primary Key,
+  author_id PRIMARY KEY,
   author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers(
-  customer_id Primary Key,
+  customer_id PRIMARY KEY,
   customer_name VARCHAR(215),
   email VARCHAR(215),
   address TEXT,
 );
 
 CREATE TABLE Orders(
-  order_id Primary Key,
+  order_id PRIMARY KEY,
   customer_id INT,
   order_date DATE,
   FOREIGN KEY (customer_id) REFERENCES (Customers)
 );
 
 CREATE TABLE Order_Details(
-  orderdetailid Primary Key,
+  orderdetailid PRIMARY KEY,
   order_id INT,
   book_id INT, 
   quantity FLOAT,
