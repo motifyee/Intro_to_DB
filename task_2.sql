9b4d23fe3743c02c591741c8dfa8db6bf534d143
mrt <iquixe@gmail.com>
2024-07-26 23:50:25 +0300

    task_2.sql

---
+++ task_2.sql
@@ -0,0 +1,39 @@
+
+
+CREATE TABLE Books(
+  book_id INT PRIMARY KEY,
+  title VARCHAR(130),
+  author_id INT, 
+  price FLOAT,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+
+CREATE TABLE Authors(
+  author_id PRIMARY KEY,
+  author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+  customer_id PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT,
+);
+
+CREATE TABLE Orders(
+  order_id PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details(
+  orderdetailid PRIMARY KEY,
+  order_id INT,
+  book_id INT, 
+  quantity DOUBLE,
+  FOREIGN KEY (book_id) REFERENCES Books(book_id),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
