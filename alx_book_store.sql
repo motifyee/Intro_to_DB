57f9fd03236bbd21275cb452e5667642de47fbcf
mrt <iquixe@gmail.com>
2024-07-26 23:37:40 +0300

    fix

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,7 +6,7 @@ CREATE TABLE Books(
   author_id INT, 
   price FLOAT,
   publication_date DATE,
-  FOREIGN KEY (author_id) REFERENCES (Authors)
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 
@@ -26,7 +26,7 @@ CREATE TABLE Orders(
   order_id PRIMARY KEY,
   customer_id INT,
   order_date DATE,
-  FOREIGN KEY (customer_id) REFERENCES (Customers)
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
@@ -34,6 +34,6 @@ CREATE TABLE Order_Details(
   order_id INT,
   book_id INT, 
   quantity FLOAT,
-  FOREIGN KEY (book_id) REFERENCES (Books),
-  FOREIGN KEY (order_id) REFERENCES (Orders)
+  FOREIGN KEY (book_id) REFERENCES Books(book_id),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id)
 );
